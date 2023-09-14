ae72033e3227d9bec5d42a066a5d6641ba3de2b0
Jonathan Liu <jwliu0724@hotmail.com>
2023-09-14 14:15:46 -0700

    Add on_message function

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ intents.message_content = True
 
 bot = discord.Client(intents=intents)
 
-command_prefix = '&&'
+command_prefix = '!'
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('-----------')
+
+@bot.event
+async def on_message(message):
+    if message.content[0] == '!':
+        # do stuff
+
 
 bot.run('MTA3MjAxOTQwMDMxNzczMDkxNw.G5g0No.a4tRzOb5NoMVMgKhcwgrSYXEFt6MnVhOCX1wQc')
