08abf659ea86cadb4ad37c5bf76ab0de086e31d1
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2023-09-18 23:11:27 -0700

    Add list of !help command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from checklist import *
 import json
 
 intents = discord.Intents.default()
@@ -16,9 +17,40 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    print(message)
-    if message.content[0] == '!':
-        await message.channel.send('it works')
+    if message.content[0] != command_prefix or message.author.id == bot.user.id:
+        return
 
+    command = message.content[1:].lower()
+    if command == 'help':
+        await message.channel.send('''
+                                   CALENDAR COMMANDS
+!calendar - see visual calendar
 
+ASSIGNMENTS COMMAND
+!save month/day 'assignment' (ex. !save 09/27 'midterm')
+!assignments - display lists of assignments due 
+!delete 'assignment' - remove assignment from calendar
+
+CHECKLIST COMMANDS
+!add 'task' - add tasks to a checklist  (ex. !add 'Do math homework')
+!finish #entry - crossess off checklist (ex. !finish #3)
+!remove #entry - remove entry from checklist
+!clear - remove all entries from checklist
+                                   
+NOTE COMMANDS
+!note 'notes' - save notes to storage (ex. !note 'this is an example')                       
+!erase #entry - remove the note at the entry number
+!wipe - erase all notes
+!edit - #entry - edit the note at the entry number
+                                   
+COURSE MANAGEMENT COMMANDS
+!courses - list courses for the week
+!add_course 'course' - add course to list (ex. !add_course 'COMP 1510')
+!remove_course 'course' - remove course from list
+!wipe_courses - remove all courses from list
+!edit_course 'course' - edit details of the course
+!next - Show next course on schedule and how much time until then
+                                ''')
+    else: 
+        await message.channel.send('Type !help to see the available commands')
 bot.run('MTE1MzUyNTMzNzY5OTkwOTY5Mg.GoyC-R.FShuoH5MVELSucNfuZ4i9bLo3tt_9P3DqmcT7A')
