94c1c941cbf1144e64187682595256f4971ff4d4
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2023-09-18 23:12:38 -0700

    separate checklist functions

---
+++ checklist.py
@@ -0,0 +1,9 @@
+async def checklist_commands(command):
+    # shave ! off of message
+    command = command.content[1:]
+
+    """ # switch case for different commands
+    match command:
+        case 'add':
+
+        case '' """
