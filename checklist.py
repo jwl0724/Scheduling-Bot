ef1371ba523eacfe8484431084d6125b06526bca
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-02 14:45:52 -0700

    finished sending message from storage

--- checklist.py
+++ checklist.py
@@ -1,6 +1,6 @@
 import csv
 
-def checklist_commands(message, command):
+async def checklist_commands(message, command):
     checklist_file = open('./storage/checklist.csv', 'r+', newline='')
     checklist_data = csv.DictReader(checklist_file)
     author_posts = [item for item in checklist_data if int(item['author']) == message.author.id]
@@ -19,10 +19,17 @@ def checklist_commands(message, command):
             pass
 
         case 'checklist':
-            pass
+            formatted_string, index = '', 0
+
+            for post in author_posts:
+                index += 1
+                content = post['content']
+                formatted_string += f'{index}. {content}\n'
+                
+            await message.channel.send(formatted_string)
 
         case _:
-            message.channel.send('Error, invalid usage. Please use !help to see proper usage')
+            await message.channel.send('Error, invalid usage. Please use !help to see proper usage')
 
     checklist_file.close()
     # csv file -> author,entry number,content
