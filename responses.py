ced61ea574198b0f342d25b9f30dfa2747be8641
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-02 10:43:56 -0700

    created new file to move responses

---
+++ responses.py
@@ -0,0 +1,29 @@
+help_response = '''
+CALENDAR COMMANDS
+!calendar - see visual calendar
+
+ASSIGNMENTS COMMAND
+!save month/day 'assignment' (ex. !save 09/27 'midterm')
+!assignments - display lists of assignments due 
+!delete 'assignment' - remove assignment from calendar
+
+CHECKLIST COMMANDS
+!add 'task' - add tasks to a checklist  (ex. !add 'Do math homework')
+!finish #entry - crosses off checklist (ex. !finish #3)
+!remove #entry - remove entry from checklist
+!clear - remove all entries from checklist
+                                   
+NOTE COMMANDS
+!note 'notes' - save notes to storage (ex. !note 'this is an example')                       
+!erase #entry - remove the note at the entry number
+!wipe - erase all notes
+!edit - #entry - edit the note at the entry number
+                                   
+COURSE MANAGEMENT COMMANDS
+!courses - list courses for the week
+!add_course 'course' - add course to list (ex. !add_course 'COMP 1510')
+!remove_course 'course' - remove course from list
+!wipe_courses - remove all courses from list
+!edit_course 'course' - edit details of the course
+!next - Show next course on schedule and how much time until then
+'''
