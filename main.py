bacab85d1645dc5d4c6fe7808be13f378e61e58e
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-02 11:07:39 -0700

    made if groups into switch case

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from API_key import API_KEY
 from checklist import *
 from static.responses import *
 from checklist import *
+from static.commands_list import *
 
 intents = discord.Intents.default()
 intents.members, intents.message_content = True, True
@@ -20,20 +21,31 @@ async def on_ready():
 async def on_message(message):
     # stop function if message isn't a command prompt or is from the bot itself
     if message.content[0] != command_prefix or message.author.id == bot.user.id: return
+
+    # get inputted command
     command = message.content[1:].lower().split()[0]
     
-    # process help command
-    if command == 'help':
-        await message.channel.send(help_response)
-        return
+    match command:
+        case 'help':
+            await message.channel.send(help_response)
 
-    # process commands under checklist
-    elif command in checklist_cmd:
-        await checklist_commands(message)
+        case command if command in CHECKLIST_CMD:
+            checklist_commands(message)
 
-    else: 
-        await message.channel.send('Type !help to see the available commands')
-        return
-    
-    checklist_commands(message)
+        case command if command in ASSIGNMENT_CMD:
+            pass
+
+        case command if command in CALENDER_CMD:
+            pass
+
+        case command if command in NOTE_CMD:
+            pass
+
+        case command if command in COURSE_CMD:
+            pass
+
+        case _:
+            await message.channel.send('Type !help to see the available commands')
+            return
+
 bot.run(API_KEY)
