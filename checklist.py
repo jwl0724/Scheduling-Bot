de214d2b62e70e4ef7e4b5a3039ab040b0831667
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-01-07 15:34:44 -0800

    add to remove and add edit lines function

--- checklist.py
+++ checklist.py
@@ -3,14 +3,14 @@ import helpers as help
 import re
 
 
-FIELDS = ('author', 'content')
+FIELDS = ('author', 'content', 'completion')
 
 
 def add_task(author_id, task):
     cleaned_task = task.replace("'", '')
     with open('./storage/checklist.csv', 'a', newline='') as file:
         file_writer = csv.DictWriter(file, fieldnames=FIELDS)
-        entry = {'author': author_id, 'content': cleaned_task}
+        entry = {'author': author_id, 'content': cleaned_task, 'completion': False}
         file_writer.__str__()
         file_writer.writerow(entry)
 
@@ -25,34 +25,30 @@ def retrieve_checklist(author_id):
 def get_task(author_id, entry_no):
     task_list = retrieve_checklist(author_id)
     try:
-        return task_list[entry_no - 1]
+        return task_list[int(entry_no) - 1]
     except IndexError:
+        return None
+    except TypeError:
         return None
 
 
-def finish_task(author_id, finished_task):  
-    with open('./storage/checklist.csv', 'a+', newline='') as file:
-        file.seek(0)
+def edit_lines(author_id, specification=None, new_content=None):
+    with open('./storage/checklist.csv', 'r', newline='') as file:
         file_reader = csv.DictReader(file, fieldnames=FIELDS)
-        for line in file_reader:
-            if int(line['author']) == author_id and line['content'] == finished_task:
-                file.write(f'{author_id},~~{finished_task}~~')
-
-
-
-
-def clear_tasks(author_id):
-    with open('./storage/checklist.csv', 'r', newline='') as read_file:
-        file_reader = csv.DictReader(read_file, fieldnames=FIELDS)
         file_data = list(file_reader)
-
-    with open('./storage/checklist.csv', 'w', newline='') as write_file:
-        file_writer = csv.DictWriter(write_file, fieldnames=FIELDS)
-
-        # rewrite lines that do not belong to the author
+    
+    with open('./storage/checklist.csv', 'w', newline='') as file:
+        file_writer = csv.DictWriter(file, fieldnames=FIELDS)
+        # rewrite lines not matching the selected for deletion
         for line in file_data:
             if int(line['author']) != author_id:
                 file_writer.writerow(line)
+            elif int(line['author']) == author_id and line['content'] == specification:
+                file_writer.writerow(new_content) if new_content != None else None
+
+
+def finish_task(author_id, finished_task):  
+    
 
 
 async def process_checklist_commands(message, command):    
@@ -68,7 +64,7 @@ async def process_checklist_commands(message, command):
             await message.channel.send('Task Added!')
 
         case 'finish':
-            regex_eval = re.search('!finish #[0-9]+')
+            regex_eval = re.search('!finish #[0-9]+', message.content)
             if not regex_eval:
                 await message.channel.send('Invalid format, please precede the entry number with #.')
                 return
@@ -76,17 +72,27 @@ async def process_checklist_commands(message, command):
             entry_no = re.search('[0-9]+', message.content).group()
             finished_task = get_task(message.author.id, entry_no)
             if not finished_task:
-                await message.channel.send('The entry number you put in doesn\'t exist, please put a valid number.')
+                await message.channel.send('Error, please input a valid number.')
                 return
 
             finish_task(message.author.id, finished_task)
             await message.channel.send('Tasked Crossed Out!')
 
         case 'remove':
-            pass
+            regex_eval = re.search("!remove #[0-9]+", message.content)
+            if not regex_eval:
+                await message.channel.send('Invalid format, please precede the entry number with #.')
+                return
+            entry_no = re.search('[0-9]+', message.content)
+            to_remove = get_task(message.author.id, entry_no)
+            if not to_remove:
+                await message.channel.send('Error, please input a valid number.')
+                return
+            
+            edit_lines(message.author.id, specification=to_remove)
 
         case 'clear':
-            clear_tasks(message.author.id)
+            edit_lines(message.author.id)
             await message.channel.send('All tasks cleared!')
 
         case 'checklist':
