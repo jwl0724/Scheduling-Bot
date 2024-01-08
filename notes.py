c530eb0795a8ce00acc2bb5983a1542cc43d316c
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-01-07 18:59:50 -0800

    fix extra spacing bug when writing to file

--- notes.py
+++ notes.py
@@ -3,15 +3,22 @@ import requests
 import os
 
 
-def write_file(author_id, file_name, notes_url):
+def get_file_path(author_id):
     file_path = os.path.join(os.path.dirname(__file__), f'storage/{author_id}')
     if not os.path.exists(file_path):
         os.makedirs(file_path)
+    return file_path
 
-    file_data = requests.request(notes_url)
-    with open(f'{author_id}_{file_name}', 'w') as file:
-        for line in file_data:
-            file.write(line)
+
+def write_file(author_id, file_name, notes_url):
+    file_path = get_file_path(author_id)
+    response = requests.get(notes_url)
+    joined_path = os.path.join(file_path, f'{author_id}_{file_name}')
+    with open(joined_path, 'w') as file:
+        notes_list = response.text.splitlines() # each line has extra /r/n, leading to extra spaces
+        for note in notes_list:
+            note += '\n'
+            file.write(note)
 
 
 async def process_notes_commands(message, command):
