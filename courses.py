a689003b27abe0f2f357de450e61e4b9be994fab
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-01-10 14:21:01 -0800

    finish saving image

--- courses.py
+++ courses.py
@@ -1,8 +1,21 @@
 import helpers as help
+from PIL import Image
 import requests
 import discord
 import os
+import io
 import re
+
+
+def save_image(author_id, img_url, file_type):
+    folder_path = help.get_file_path(author_id, 'schedule')
+    if len(os.listdir(folder_path)) > 0:
+        to_delete = os.path.join(folder_path, os.listdir(folder_path)[0])
+        os.remove(to_delete)
+    file_path = os.path.join(folder_path, f"schedule.{file_type.replace('image/', '')}") 
+    image_data = requests.get(img_url).content
+    image = Image.open(io.BytesIO(image_data))
+    image.save(file_path)
 
 
 async def process_course_commands(message, command):
@@ -17,7 +30,7 @@ async def process_course_commands(message, command):
             if 'image' not in message.attachments[0].content_type:
                 await message.channel.send('Please upload an image file')
                 return
-            
+            save_image(message.author.id, message.attachments[0].url, message.attachments[0].content_type)
             
         case 'courses':
             pass
