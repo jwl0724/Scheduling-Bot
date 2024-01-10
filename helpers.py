d32ab492e6a88081553a5dc30066030912a2a1a1
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-01-10 13:25:56 -0800

    convert get_file_path into global helper function

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+import os
+
 def get_entry_number(message_content):
     try:
         convert_to_int = int(message_content[9:])
@@ -5,6 +7,15 @@ def get_entry_number(message_content):
     except ValueError:
         return 0
     
+
+def get_file_path(author_id, sub_folder, file_name=None):
+    if file_name:
+        sub_folder += f'/{file_name}'
+    file_path = os.path.join(os.path.dirname(__file__), f'storage/{author_id}/{sub_folder}')
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    return file_path
+
 
 def get_command(message_content):
     command = message_content[1:].lower().split()[0]
