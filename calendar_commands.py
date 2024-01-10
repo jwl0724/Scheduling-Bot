325d781d965d46ff45517cf9cb34840460512bec
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-01-10 09:16:13 -0800

    finish month functionality

--- calendar_commands.py
+++ calendar_commands.py
@@ -4,6 +4,7 @@ import calendar
 
 async def process_calendar_commands(message, command):
     current_year = date.today().year
+    current_month = date.today().month
     match command:
         case 'calendar':
             await message.channel.send(f'{calendar.calendar(current_year)}')
@@ -15,7 +16,7 @@ async def process_calendar_commands(message, command):
                 7: 'July', 8: 'August', 9: 'September',
                 10: 'October', 11: 'November', 12: 'December'
             }
-            await message.channel.send(f'Today is {month_key[date.today().month]} {date.today().day} {current_year}')
+            await message.channel.send(f'Today is {month_key[current_month]} {date.today().day} {current_year}')
 
         case 'month':
-            pass
+            await message.channel.send(f'{calendar.month(current_year, current_month)}')
